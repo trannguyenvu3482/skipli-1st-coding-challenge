273c4aa4be019162d46aeb71d5ad775d8752f053
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2023-02-07 22:41:37 +0700

    added api calls using axios

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { Box, Button, Container, TextField } from '@mui/material';
+import axios from 'axios';
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,20 +9,50 @@ const Login = () => {
   const [phone, setPhone] = useState('');
   const [code, setCode] = useState('');
 
-  const handleSendCode = (e) => {
-    if (phone === '') {
-      toast.error('Please enter a phone number');
-    } else {
-      toast.success('A six-digit code has been sent to your device.');
+  const handleSendCode = async (e) => {
+    try {
+      if (phone === '') {
+        toast.error('Please enter a phone number');
+      } else {
+        const res = await axios.post('http://localhost:3001/api/create-code', {
+          phoneNumber: phone,
+        });
+
+        console.log(res.data);
+
+        if (res.status === 200) {
+          toast.success('A code has been sent to your phone!');
+        }
+      }
+    } catch (error) {
+      toast.error(
+        'Something went wrong, please check your phone number and try again!'
+      );
     }
   };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    if (phone === '' || code === '') {
-      toast.error('Please enter all the required fields');
-    } else {
-      toast.success('Complete registered!');
+  const handleFormSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      if (phone === '' || code === '') {
+        toast.error('Please enter all the required fields');
+      } else {
+        const res = await axios.post(
+          'http://localhost:3001/api/validate-code',
+          {
+            phoneNumber: phone,
+            accessCode: code,
+          }
+        );
+
+        console.log(res.data);
+
+        if (res.status === 200) {
+          toast.success('You have successfully logged in!');
+        }
+      }
+    } catch (error) {
+      toast.error('Your code is incorrect, please try again!');
     }
   };
 
@@ -34,7 +65,6 @@ const Login = () => {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          margin: '20px',
         }}
       >
         <h1>Register</h1>
