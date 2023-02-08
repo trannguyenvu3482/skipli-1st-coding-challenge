df30b07e52906607e294d52a32ee9eb08c08d6e3
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2023-02-08 15:04:57 +0700

    added creds to env

--- server/firebase.js
+++ server/firebase.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./creds.json');
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 initializeApp({
   credential: cert(serviceAccount),
