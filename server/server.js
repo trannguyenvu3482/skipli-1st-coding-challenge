b4f750fb1b57a41fdaa3a96e2ba348b48a7e48bf
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2023-02-08 14:59:39 +0700

    fix the port of server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Start server
 app.listen(port, () => {
