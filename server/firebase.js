2d1137a5d570c39fe1094d4ee289b0a7cf32c0e0
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2023-02-08 19:53:33 +0700

    change firebase key from using json to env

--- server/firebase.js
+++ server/firebase.js
@@ -1,7 +1,10 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+// Get the environment variable FIREBASE_PRIVATE_KEY, base64 decode it and parse it as JSON
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('ascii')
+);
 
 initializeApp({
   credential: cert(serviceAccount),
